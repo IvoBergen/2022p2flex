5ca1e8bebc352c58458be24d47d75d3f7cf877dd
Ivo <34885@ma-web.nl>
2022-12-21 14:41:09 +0100

    t/m les 9 gemaakt!

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 class Player
 {
     constructor(index)
@@ -7,11 +6,9 @@ class Player
         this.atTile = 0;
         this.pawn = document.getElementsByClassName("pawn" + index)[0];
         this.pawn.style.display = "block";
-
     }
 }
-
-class tile
+class Tile
 {
     constructor(div)
     {
@@ -19,22 +16,98 @@ class tile
         this.goto = -1;
     }
 }
-
 class Game
-{
-    consturctor()
+{   
+    constructor(div)
     {
-        this.selectplayersDiv = document.getElementsByClassName("selectplayers")[0];
+        this.selectplayersDiv = document.getElementsByClassName("selectPlayers")[0];
         this.winnerDiv = document.getElementsByClassName("winner")[0];
-        this.playerturnDiv = document.getElementsByClassName("playerturn")[0];
+        this.playerturnDiv = document.getElementsByClassName("playerTurn")[0];
         this.rollDiv = document.getElementsByClassName("roll")[0];
         this.mainDiv = document.getElementsByClassName("main")[0];
         this.boardDiv = document.getElementsByClassName("board")[0];
-        this.boardoverlayDiv = document.getElementsByClassName("boardoverlay")[0];
+        this.boardoverlayDiv = document.getElementsByClassName("selectPlayers")[0];
 
-        this.tile = [];
+        this.tiles = [];
         this.players = [];
-        this.playerturn = 0;
+        this.playerTurn = 0;
+        this.setupBoard();
     }
-    
+    setupBoard()
+    {   
+        let path = [1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+        let x = 0;
+        let y = 10;
+        let tileSize = 55;
+        for (var i = 0; i < path.length; i++)
+        {
+            let cmd = path[i];
+            //right movement map
+            if(cmd == 1){
+                x++;
+            }
+            // left movement map
+            else if (cmd == 3){
+                x--;
+            }
+            // up movement map 
+            else if (cmd == 0){
+                y--;
+            }
+            let div = this.makeBoardDiv(x * tileSize ,y * tileSize,i+1)
+            let tile = new Tile(div);
+            this.tiles.push(tile);
+
+        }
+        this.setupGotos();
+    }
+
+    setupGotos()
+    {   //let op! deze tegelnummers beginnen 1
+        let goto = [[6, 14], [16, 4], [17, 23], [27, 33], [29, 10], [38, 43], [39, 20], [45, 34]];
+        for (var i = 0; i < goto.length; i++)
+        {
+            let element = goto[i];
+            let start = element[0] - 1;
+            let end = element[1] - 1;
+
+            let tile = this.tiles[start];
+            tile.goto = end;
+        }
+    }
+    start(amountOfPlayers)
+    {
+
+    }
+    moveToNextPlayer()
+    {
+
+    }
+    draw()
+    {
+
+    }
+    roll()
+    {
+
+    }
+    setPawn(playerI, atTile)
+    {
+
+    }
+    makeBoardDiv(x,y,tileDisplayNumber)
+    {
+        let div = document.createElement("div");
+        div.className = "tile";
+        div.style.left = x + "px";
+        div.style.top = y + "px";
+        div.textContent = tileDisplayNumber;      
+        this.boardDiv.appendChild(div);
+
+        return div;
+    }
+}
+function init()
+{
+    let game = new Game();
 }
